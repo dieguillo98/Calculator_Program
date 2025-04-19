f266dc83725c75b0a0acd634fdb27abb9c1819a9
mrcoder1998 <dl491945@gmail.com>
2025-04-19 12:01:04 -0600

    code modified

--- acertar_el_numero.py
+++ acertar_el_numero.py
@@ -1,18 +1,18 @@
 import random
-numero_secreto = random.randint(1,1000)
+secret_number = random.randint(1,1000)
 
-acierto = False
+success = False
 
-for intentos in range(1,11,1):
-    print(f'Intento numero: {intentos}')
-    numero_ingresado = int(input('Ingrese el valor que piensa que es el numero a adivinar: '))
-    if numero_ingresado > numero_secreto:
-        print("Demasiado alto")
-    elif numero_ingresado < numero_secreto:
-        print('Demasiado bajo')
+for attempts in range(1,11,1):
+    print(f'Attempt number: {attempts}')
+    guessing_number = int(input('Enter the number to guess: '))
+    if guessing_number > secret_number:
+        print("High number")
+    elif guessing_number < secret_number:
+        print('Low number')
     else:
-        print(f'EXITO!!!... Lo adivinaste en el intento numero: {intentos}')
-        acierto = True
+        print(f'CONGRATULATIONS!!, yout guest the number in the attempt number: {attempts}')
+        success = True
         break
-if intentos == 10 and not acierto:
-    print(f'ERROR!!!... No pudiste adivinar, el numero secreto era: {numero_secreto}')
+if attempts == 10 and not success:
+    print(f'ERROR!!!... You cannot guess the number, the secret number was: {secret_number}')
